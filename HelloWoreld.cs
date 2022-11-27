dd710e52bcaf106c229ff815c56859e5f72289fe
hecita2022 <119161072+hecita2022@users.noreply.github.com>
2022-11-27 15:14:46 +0500

    Add files via upload

---
+++ HelloWoreld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWoreld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // оманда выводит "привет мир"
+        //на аглиском
+        print("Hello, World");
+        print(2022);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
